1bb97001000ae3dec07bd6c9dc78f27afa88c59a
Komal-Kumari789 <codekomalk02024@gmail.com>
2026-09-09 22:27:41 +0530

    Time: 10 ms (77.16%) | Memory: 47.2 MB (76.30%) - LeetSync

---
+++ 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+        Map<Integer,Integer> count=new HashMap<>();
+        for(int i: deck){
+            count.put(i,count.getOrDefault(i,0)+1);
+        }
+        int res=0;
+        for(int i:count.values()){
+            res=gcd(i,res);
+        }
+        return res>1;
+    }
+    public int gcd(int a,int b){
+        return b>0 ? gcd(b,a%b) : a;
+    }
+}
