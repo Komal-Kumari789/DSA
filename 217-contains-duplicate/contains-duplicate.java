03ec49ad662f78a9409b85c969d8da2e66233200
Komal-Kumari789 <codekomalk02024@gmail.com>
2026-09-24 22:23:20 +0530

    Time: 24 ms (28.29%) | Memory: 81.3 MB (96.65%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+        // boolean res=false;
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[i]==nums[j]){
+        //             res=true;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return res;
+    }
+}
